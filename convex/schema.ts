b4483cb203db3a68b9142815b1ee93a45808dd02
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2024-08-31 04:28:38 -0700

    update reports schema

--- convex/schema.ts
+++ convex/schema.ts
@@ -12,6 +12,12 @@ export default defineSchema({
     body: v.string(),
   }),
   reports: defineTable({
-    storageId: v.id("_storage"),
+    storageId: v.id("_storage"), // File ref for raw data
+    validDate: v.string(), // Date of forecast
+    dataType: v.string(), // (F)orecast or (O)bservation
+    reportingArea: v.string(), // Key for zip lookups
+    parameterName: v.string(), // OZONE, PM2.5, etc.
+    aqiValue: v.string(), // The # measure itself
+    aqiCategory: v.string(), // The AQI rating: "Good", "Moderate", "Unhealthy", etc.
   }),
 });
