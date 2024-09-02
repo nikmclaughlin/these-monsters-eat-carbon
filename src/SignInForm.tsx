355aff44915beac09f8bd5c22488625d9e93bdc0
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2024-09-02 04:12:11 -0700

    remove email auth

--- src/SignInForm.tsx
+++ src/SignInForm.tsx
@@ -1,8 +1,4 @@
-import { SignInMethodDivider } from "@/components/SignInMethodDivider";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Toaster } from "@/components/ui/toaster";
-import { useToast } from "@/components/ui/use-toast";
 import { useAuthActions } from "@convex-dev/auth/react";
 import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { useState } from "react";
@@ -19,8 +15,6 @@ export function SignInForm() {
               Sign in or create an account
             </h2>
             <SignInWithGitHub />
-            <SignInMethodDivider />
-            <SignInWithMagicLink handleLinkSent={() => setStep("linkSent")} />
           </>
         ) : (
           <>
@@ -53,37 +47,5 @@ export function SignInWithGitHub() {
     >
       <GitHubLogoIcon className="mr-2 h-4 w-4" /> GitHub
     </Button>
-  );
-}
-
-function SignInWithMagicLink({
-  handleLinkSent,
-}: {
-  handleLinkSent: () => void;
-}) {
-  const { signIn } = useAuthActions();
-  const { toast } = useToast();
-  return (
-    <form
-      className="flex flex-col"
-      onSubmit={(event) => {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        signIn("resend", formData)
-          .then(handleLinkSent)
-          .catch((error) => {
-            console.error(error);
-            toast({
-              title: "Could not send sign-in link",
-              variant: "destructive",
-            });
-          });
-      }}
-    >
-      <label htmlFor="email">Email</label>
-      <Input name="email" id="email" className="mb-4" autoComplete="email" />
-      <Button type="submit">Send sign-in link</Button>
-      <Toaster />
-    </form>
   );
 }
